91678e796820e798f67837102d9939cb21ccae18
Mohith <mohithbalsara@outlook.com>
2017-01-02 13:42:06 +0530

    Validation added

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,11 +3,18 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+	title: 'abc'
+	description: ''
+	image_url: 'abc.fp'
+	price: 20.00
+	}
   end
 
   test "should get index" do
     get products_url
     assert_response :success
+	assert_not_nil assigns(:products)
   end
 
   test "should get new" do
@@ -17,8 +24,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    end
+      #post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+post :create, product: @update    
+end
 
     assert_redirected_to product_url(Product.last)
   end
@@ -34,7 +42,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+put: update, id: @product, product: @update   
+ #patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
     assert_redirected_to product_url(@product)
   end
 
